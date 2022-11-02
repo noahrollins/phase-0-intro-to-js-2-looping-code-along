585d83503e2c14e600a059bd65ee920ad190516e
Noah Rollins <noahjrollins@gmail.com>
2022-11-01 19:07:11 -0500

    commit tree c7e7451c0bebf87e8e3b2ac2773482be0421ffa9

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(giftors, msg) {
+    const cards = [];
+    for (let i = 0; i < giftors.length; i++) {
+       cards.push(`Thank you, ${giftors[i]}, for the wonderful ${msg} gift!`)
+    }
+    return cards
+}   
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number--)
+    }
+    console.log(number)
+}
+
+
